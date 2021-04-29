e6860704b1b768c4c1958cac1b54d5877ede3b36
Irfan Saputra <irfan.saputra2035@students.unila.ac.id>
2021-04-29 21:50:13 +0700

    Horizontal
    Menemukan horizontal

--- petrukFindwords.cpp
+++ petrukFindwords.cpp
@@ -20,20 +20,34 @@ const int cols = 16, rows = 15;
                                 "pdcrzmsngrdnrpz",
                                 "ohnkzwaterjgtra"};
 
-char *getWordVertical(int);
-char *reverse(char *);
-bool searchVertical(char *);
-bool searchHorizontal(char *);
+
+char *getWordHorizontal(int );
+
+bool searchHorizontal(char *word){
+    bool found;
+    char *store ;
+    for (int i = 0 ;i<rows;i++){
+        store = *(words+i);
+        if(strstr(store,word)){
+            found = true;
+           }
+        else {
+            found = false;
+        }
+    }
+    return found;
+}
 
 
 int main()
 {
     char word[16];
     int n;
-    cin>>n;
+    cin >> n;
+    cin.ignore(n);
     for (int i=0;i<n;i++){
         cin.getline(word,16,'\n');
-        if (searchVertical(word) || searchHorizontal(word))
+        if (searchHorizontal(word) )
             cout << "Ada\n";
         else
             cout << "Tidak Ada\n";
